94c2f1c93ec0624ffdca2906a708f2e7287dd1ee
François Michonneau <francois.michonneau@gmail.com>
2020-09-27 08:43:43 +0200

    initial commit

---
+++ R/comparer.R
@@ -0,0 +1,56 @@
+##' @importFrom fs dir_ls
+cpr_list_files <- function(path, recurse = TRUE, type = "file", ...) {
+  fs::dir_ls(path, recurse = recurse, type = type, ...)
+}
+
+##' @importFrom tools md5sum
+cpr_md5sum <- function(files) {
+  tools::md5sum(files)
+}
+
+##' Compares whether the content of the files in two directory are identical.
+##' This can be useful when you cannot rely on the timestamps or the size of the
+##' files to know if their contents have changed.
+##'
+##' By default, to compare the content of the files, the following arguments are
+##' passed to `fs::dir_ls`: `recurse = TRUE` and `type = "file"`.
+##' @title Compare the files in two folders
+##' @param path_1 path of the first folder to compare
+##' @param path_2 path of the second folder to compare
+##' @param ... additional arguments to be passed to `fs::dir_ls` (see details)
+##' @return a tibble
+##' @export
+##' @importFrom fs path_tidy path_common
+##' @importFrom tidyr pivot_wider
+##' @importFrom dplyr mutate
+##' @importFrom rlang .data
+compare_folders <- function(path_1, path_2, ...) {
+
+  path_1 <- normalizePath(fs::path_tidy(path_1))
+  path_2 <- normalizePath(fs::path_tidy(path_2))
+
+  cnt <- purrr::map(
+    list(path_1, path_2),
+    cpr_list_files,
+    ...
+  )
+
+  purrr::map_df(
+    cnt,
+    function(x) {
+      tibble::tibble(
+        path = fs::path_common(fs::path_dir(x)),
+        files = gsub(.data$path, "", x),
+        md5 = cpr_md5sum(x)
+      )
+    }
+  ) %>%
+    tidyr::pivot_wider(
+      names_from = .data$path,
+      values_from = .data$md5
+    ) %>%
+    dplyr::mutate(
+      identical = .data[[path_1]] == .data[[path_2]]
+    )
+
+}
